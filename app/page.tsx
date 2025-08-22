47b19fdfb8d4bca35e8e84c1ef8ba224835d76da
RuiTakahashi <ruitakahashi@RuiTakahashinoMacBook-Air.local>
2025-08-22 16:01:42 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,9 @@ export default function Home() {
           <li className="tracking-[-.01em]">
             Save and see your changes instantly.
           </li>
+          <li className="tracking-[-.01em]">
+            This is a new todo app.
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
